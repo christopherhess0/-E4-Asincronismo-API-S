5b5625ed84304f755c7464f5d718adb5e38fd6dc
christopherhess0 <83043287+christopherhess0@users.noreply.github.com>
2024-10-19 19:23:06 -0300

    Added getMaxPokemons.js to valid into getData function for valid quantity of pokemons in the input

--- API/data.js
+++ API/data.js
@@ -1,19 +1,30 @@
 import { captureValue } from "../Functions/captureValue.js";
+import { getMaxPokemons } from "../Functions/getMaxPokemons.js";
+
 
 export const getData = async () => {
-    const id = captureValue()
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const id = captureValue();
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
     try {
+        const maxPoke = await getMaxPokemons();        
+        if (maxPoke === null || maxPoke === undefined) {
+            alert("Hubo un error al obtener el número máximo de pokemones.");
+            return;
+        }
+       if (id > maxPoke) {
+            alert("El id ingresado es mayor al máximo de pokemones");
+            return;
+        }
+
         const res = await fetch(url);
         if (!res.ok) {
             throw new Error('Error al obtener los datos de la API');
         }
         const pokeObject = await res.json();
-        return pokeObject
+        return pokeObject;
     } catch (e) {
         console.error('Hubo un error:', e);
     }
-}
-
-
+};
 
---
+++ Functions/getMaxPokemons.js
@@ -0,0 +1,12 @@
+export const getMaxPokemons = async () => {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1');
+        const data = await response.json();
+        const numeroMaxPokemones = data.count;
+        console.log("Número máximo de pokemones:", numeroMaxPokemones);
+        return numeroMaxPokemones;
+    } catch (error) {
+        console.error('Error al obtener los Pokémon:', error);
+        return null;
+    }
+};
